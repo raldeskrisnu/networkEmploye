c93738861aa6d893f3b68b5b908332c1ce37f783
ariiyantto168 <ariiyantto168@gmail.com>
2020-05-29 21:58:28 +0700

    updated

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use('/api/posts', require('./routes/api/posts'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/public'));
+  app.use(express.static('client/src'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
