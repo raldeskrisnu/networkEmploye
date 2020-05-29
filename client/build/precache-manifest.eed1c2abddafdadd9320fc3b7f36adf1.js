0922e02f0b777e3faa35f020c70f7b7953bfc1e3
ariiyantto168 <ariiyantto168@gmail.com>
2020-05-29 22:29:08 +0700

    update prod mode

---
+++ client/build/precache-manifest.eed1c2abddafdadd9320fc3b7f36adf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "260d12326330f25b9bfefff387b7c0cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ee8bf343c05776f20e2",
+    "url": "/static/css/main.28ec31e2.chunk.css"
+  },
+  {
+    "revision": "c197f6111def9dc08c70",
+    "url": "/static/js/2.ad571d88.chunk.js"
+  },
+  {
+    "revision": "d4773cfc8952e56a2df4dd3d885d1f23",
+    "url": "/static/js/2.ad571d88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ee8bf343c05776f20e2",
+    "url": "/static/js/main.3ae24fde.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
